3931df767d58ea9720e26133b9135cd74f65f2d7
Trevor Starick <trevor.starick@gmail.com>
2014-06-26 13:46:47 -0400

    Now using un minified bundle

--- dist/dfjs.js
+++ dist/dfjs.js
@@ -10,7 +10,8 @@ Settings = {
   size: Math.ceil(window.innerWidth / 80), // size in pixels of block
   maxSize: 8,
   width: 80, // size in pixels of width
-  height: 45 // size in pixels of height
+  height: 45, // size in pixels of height
+  seed: Math.ceil(Math.random() * Date.now()).toString(36).substring(0, 8),
 };
 
 Settings.updateSize = function() {
@@ -20,6 +21,7 @@ Settings.updateSize = function() {
   console.log('Size updated...', Settings.size);
 };
 
+window.document.title += ' ' + Settings.seed;
 console.log('%c%c background: #222; color: #bada55', 'background: #222; color: #bada55');
 
 // Start Game
@@ -30,6 +32,10 @@ Game.start();
 'use strict';
 
 var Entity = {};
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 // @ENTITY_MAIN
 Entity.createNew = function(data, pos, cb) {
@@ -44,6 +50,39 @@ Entity.createNew = function(data, pos, cb) {
     return cb(true);
   } else {
     return cb(false);
+  }
+};
+
+Entity.spawn = function(type, data) {
+  data = data || {};
+  var spawned = 0;
+
+  function increase(success) {
+    if (success) spawned += 1;
+  }
+  while (spawned < 1) {
+    var x = getRandomInt(0, 80 - 1),
+      y = getRandomInt(0, 45 - 1);
+    Entity.createNew({
+      type: type,
+      data: data
+    }, [x, y], increase);
+  }
+};
+
+Entity.populate = function(type, data, limit) {
+  var spawned = 0;
+
+  function increase(success) {
+    if (success) spawned += 1;
+  }
+  while (spawned < limit) {
+    var x = getRandomInt(0, 80 - 1),
+      y = getRandomInt(0, 45 - 1);
+    Entity.createNew({
+      type: type,
+      data: data
+    }, [x, y], increase);
   }
 };
 
@@ -65,7 +104,7 @@ Entity.MoveDown = function(id) {
 };
 
 Entity.MoveEntity = function(dirX, dirY, id) {
-  if (Game._interval && !Game.paused) {
+  if (Game._interval && Game.status !== 'paused') {
     var that = Game.entityMap[id];
     var cord = that.coordinates;
 
@@ -85,27 +124,32 @@ Entity.MoveEntity = function(dirX, dirY, id) {
 
 module.exports = Entity;
 },{}],4:[function(require,module,exports){
+'use strict';
+
 window.Game = {};
 
 var Map = require('./map.js'),
   Entity = require('./entities.js'),
   AI = require('./ai.js'),
-  Physics = require('./physics.js');
+  Physics = require('./physics.js'),
+  MersenneTwister = require('./mersennetwister');
 
 Game = {
   map: [],
   entityMap: {},
-  version: 1
+  version: 1,
+  state: 'paused'
 };
 
-var keysSet = false;
+var seed = parseInt(Settings.seed, 36);
+var m = new MersenneTwister(seed);
+console.log(seed, m.random());
 
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+var keysSet = false; // Still not sure what this is for
 
 // @GAME
 Game.init = function() {
+
   if (Settings.size > Settings.maxSize) {
     Settings.size = Settings.maxSize;
   }
@@ -117,12 +161,12 @@ Game.init = function() {
 
   Map.init();
 
-  Game.spawn('player');
+  Entity.spawn('player');
 
-  Game.populate('liquid', {}, 10);
-  Game.populate('beast', {}, 7);
-  Game.populate('npc', {}, 15);
-  Game.populate('block', {}, 100);
+  Entity.populate('liquid', {}, 10);
+  Entity.populate('beast', {}, 7);
+  Entity.populate('npc', {}, 15);
+  Entity.populate('block', {}, 100);
 
   if (!keysSet) {
     $(document).keydown(function(event) {
@@ -143,44 +187,8 @@ Game.init = function() {
     });
     keysSet = true;
   }
+  console.log('Game initialized...');
 };
-
-// Initial populating/generation
-
-Game.populate = function(type, data, limit) {
-  var spawned = 0;
-
-  function increase(success) {
-    if (success) spawned += 1;
-  }
-  while (spawned < limit) {
-    var x = getRandomInt(0, 80 - 1),
-      y = getRandomInt(0, 45 - 1);
-    Entity.createNew({
-      type: type,
-      data: data
-    }, [x, y], increase);
-  }
-};
-
-Game.spawn = function(type, data) {
-  data = data || {};
-  var spawned = 0;
-
-  function increase(success) {
-    if (success) spawned += 1;
-  }
-  while (spawned < 1) {
-    var x = getRandomInt(0, 80 - 1),
-      y = getRandomInt(0, 45 - 1);
-    Entity.createNew({
-      type: type,
-      data: data
-    }, [x, y], increase);
-  }
-};
-
-// Per tick
 
 Game.update = function() {
   window.stats.update();
@@ -218,25 +226,13 @@ Game.draw = function() {
   }
 };
 
-// Controls
-
-Game.run = function() {
-  if (Game.map.length) {
-    Game.update();
-    Game.draw();
-  } else {
-    Game.init();
-    console.log('Game initialized...');
-  }
-};
-
 Game.pause = function() {
-  paused = true;
+  Game.state = 'paused';
   clearInterval(Game._interval);
 };
 
 Game.start = function() {
-  paused = false;
+  Game.state = 'running';
   if (Game._interval) {
     clearInterval(Game._interval);
   }
@@ -257,8 +253,19 @@ Game.reset = function() {
   Game.start();
 };
 
+Game.run = function() {
+  if (Game.map.length) {
+    Game.update();
+    Game.draw();
+  } else {
+    Game.init();
+  }
+};
+
 module.exports = Game;
-},{"./ai.js":2,"./entities.js":3,"./map.js":5,"./physics.js":6}],5:[function(require,module,exports){
+},{"./ai.js":2,"./entities.js":3,"./map.js":5,"./mersennetwister":6,"./physics.js":7}],5:[function(require,module,exports){
+'use strict';
+
 var Map = function() {
   this.initialized = false;
 };
@@ -283,5 +290,223 @@ Map.prototype = {
 
 module.exports = new Map();
 },{}],6:[function(require,module,exports){
+'use strict';
+
+/*
+ I've wrapped Makoto Matsumoto and Takuji Nishimura's code in a namespace
+ so it's better encapsulated. Now you can have multiple random number generators
+ and they won't stomp all over eachother's state.
+
+ If you want to use this as a substitute for Math.random(), use the random()
+ method like so:
+
+ var m = new MersenneTwister();
+ var randomNumber = m.random();
+
+ You can also call the other genrand_{foo}() methods on the instance.
+
+ If you want to use a specific seed in order to get a repeatable random
+ sequence, pass an integer into the constructor:
+
+ var m = new MersenneTwister(123);
+
+ and that will always produce the same random sequence.
+
+ Sean McCullough (banksean@gmail.com)
+ */
+
+/*
+ A C-program for MT19937, with initialization improved 2002/1/26.
+ Coded by Takuji Nishimura and Makoto Matsumoto.
+
+ Before using, initialize the state by using init_genrand(seed)
+ or init_by_array(init_key, key_length).
+
+ Copyright (C) 1997 - 2002, Makoto Matsumoto and Takuji Nishimura,
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions
+ are met:
+
+ 1. Redistributions of source code must retain the above copyright
+ notice, this list of conditions and the following disclaimer.
+
+ 2. Redistributions in binary form must reproduce the above copyright
+ notice, this list of conditions and the following disclaimer in the
+ documentation and/or other materials provided with the distribution.
+
+ 3. The names of its contributors may not be used to endorse or promote
+ products derived from this software without specific prior written
+ permission.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+ Any feedback is very welcome.
+ http://www.math.sci.hiroshima-u.ac.jp/~m-mat/MT/emt.html
+ email: m-mat @ math.sci.hiroshima-u.ac.jp (remove space)
+ */
+
+var MersenneTwister = function(seed) {
+  if (seed == undefined) {
+    seed = new Date().getTime();
+  }
+  /* Period parameters */
+  this.N = 624;
+  this.M = 397;
+  this.MATRIX_A = 0x9908b0df;
+  /* constant vector a */
+  this.UPPER_MASK = 0x80000000;
+  /* most significant w-r bits */
+  this.LOWER_MASK = 0x7fffffff;
+  /* least significant r bits */
+
+  this.mt = new Array(this.N);
+  /* the array for the state vector */
+  this.mti = this.N + 1;
+  /* mti==N+1 means mt[N] is not initialized */
+
+  this.init_genrand(seed);
+};
+
+/* initializes mt[N] with a seed */
+MersenneTwister.prototype.init_genrand = function(s) {
+  this.mt[0] = s >>> 0;
+  for (this.mti = 1; this.mti < this.N; this.mti++) {
+    var s = this.mt[this.mti - 1] ^ (this.mt[this.mti - 1] >>> 30);
+    this.mt[this.mti] = (((((s & 0xffff0000) >>> 16) * 1812433253) << 16) + (s & 0x0000ffff) * 1812433253) + this.mti;
+    /* See Knuth TAOCP Vol2. 3rd Ed. P.106 for multiplier. */
+    /* In the previous versions, MSBs of the seed affect   */
+    /* only MSBs of the array mt[].                        */
+    /* 2002/01/09 modified by Makoto Matsumoto             */
+    this.mt[this.mti] >>>= 0;
+    /* for >32 bit machines */
+  }
+};
+
+/* initialize by an array with array-length */
+/* init_key is the array for initializing keys */
+/* key_length is its length */
+/* slight change for C++, 2004/2/26 */
+MersenneTwister.prototype.init_by_array = function(init_key, key_length) {
+  var i, j, k;
+  this.init_genrand(19650218);
+  i = 1;
+  j = 0;
+  k = (this.N > key_length ? this.N : key_length);
+  for (; k; k--) {
+    var s = this.mt[i - 1] ^ (this.mt[i - 1] >>> 30)
+    this.mt[i] = (this.mt[i] ^ (((((s & 0xffff0000) >>> 16) * 1664525) << 16) + ((s & 0x0000ffff) * 1664525))) + init_key[j] + j;
+    /* non linear */
+    this.mt[i] >>>= 0;
+    /* for WORDSIZE > 32 machines */
+    i++;
+    j++;
+    if (i >= this.N) {
+      this.mt[0] = this.mt[this.N - 1];
+      i = 1;
+    }
+    if (j >= key_length) j = 0;
+  }
+  for (k = this.N - 1; k; k--) {
+    var s = this.mt[i - 1] ^ (this.mt[i - 1] >>> 30);
+    this.mt[i] = (this.mt[i] ^ (((((s & 0xffff0000) >>> 16) * 1566083941) << 16) + (s & 0x0000ffff) * 1566083941)) - i;
+    /* non linear */
+    this.mt[i] >>>= 0;
+    /* for WORDSIZE > 32 machines */
+    i++;
+    if (i >= this.N) {
+      this.mt[0] = this.mt[this.N - 1];
+      i = 1;
+    }
+  }
+
+  this.mt[0] = 0x80000000;
+  /* MSB is 1; assuring non-zero initial array */
+};
+
+/* generates a random number on [0,0xffffffff]-interval */
+MersenneTwister.prototype.genrand_int32 = function() {
+  var y;
+  var mag01 = new Array(0x0, this.MATRIX_A);
+  /* mag01[x] = x * MATRIX_A  for x=0,1 */
+
+  if (this.mti >= this.N) { /* generate N words at one time */
+    var kk;
+
+    if (this.mti == this.N + 1) /* if init_genrand() has not been called, */
+      this.init_genrand(5489);
+    /* a default initial seed is used */
+
+    for (kk = 0; kk < this.N - this.M; kk++) {
+      y = (this.mt[kk] & this.UPPER_MASK) | (this.mt[kk + 1] & this.LOWER_MASK);
+      this.mt[kk] = this.mt[kk + this.M] ^ (y >>> 1) ^ mag01[y & 0x1];
+    }
+    for (; kk < this.N - 1; kk++) {
+      y = (this.mt[kk] & this.UPPER_MASK) | (this.mt[kk + 1] & this.LOWER_MASK);
+      this.mt[kk] = this.mt[kk + (this.M - this.N)] ^ (y >>> 1) ^ mag01[y & 0x1];
+    }
+    y = (this.mt[this.N - 1] & this.UPPER_MASK) | (this.mt[0] & this.LOWER_MASK);
+    this.mt[this.N - 1] = this.mt[this.M - 1] ^ (y >>> 1) ^ mag01[y & 0x1];
+
+    this.mti = 0;
+  }
+
+  y = this.mt[this.mti++];
+
+  /* Tempering */
+  y ^= (y >>> 11);
+  y ^= (y << 7) & 0x9d2c5680;
+  y ^= (y << 15) & 0xefc60000;
+  y ^= (y >>> 18);
+
+  return y >>> 0;
+};
+
+/* generates a random number on [0,0x7fffffff]-interval */
+MersenneTwister.prototype.genrand_int31 = function() {
+  return (this.genrand_int32() >>> 1);
+};
+
+/* generates a random number on [0,1]-real-interval */
+MersenneTwister.prototype.genrand_real1 = function() {
+  return this.genrand_int32() * (1.0 / 4294967295.0);
+  /* divided by 2^32-1 */
+};
+
+/* generates a random number on [0,1)-real-interval */
+MersenneTwister.prototype.random = function() {
+  return this.genrand_int32() * (1.0 / 4294967296.0);
+  /* divided by 2^32 */
+};
+
+/* generates a random number on (0,1)-real-interval */
+MersenneTwister.prototype.genrand_real3 = function() {
+  return (this.genrand_int32() + 0.5) * (1.0 / 4294967296.0);
+  /* divided by 2^32 */
+};
+
+/* generates a random number on [0,1) with 53-bit resolution*/
+MersenneTwister.prototype.genrand_res53 = function() {
+  var a = this.genrand_int32() >>> 5,
+    b = this.genrand_int32() >>> 6;
+  return (a * 67108864.0 + b) * (1.0 / 9007199254740992.0);
+};
+/* These real versions are due to Isaku Wada, 2002/01/09 added */
+
+//Export the Browserify module
+module.exports = MersenneTwister;
+},{}],7:[function(require,module,exports){
 module.exports=require(2)
-},{}]},{},[1]);
+},{}]},{},[1])
